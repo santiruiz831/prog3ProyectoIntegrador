c1092c3724c3f1a4f43da694473606a16008d018
Tatiana <69600097+TatiKatz@users.noreply.github.com>
2022-04-10 22:50:35 -0300

    ver mas

--- tmbd/src/components/Card/Card.js
+++ tmbd/src/components/Card/Card.js
@@ -5,27 +5,54 @@ class Card extends Component{
 constructor(props){
     super(props)
     this.state={
-        link: "https://image.tmdb.org/t/p/w342"
-    }
+        link: "https://image.tmdb.org/t/p/w342",
+        text: "Ver más",
+        viewMore: false,
+    };
+
 }
+
+    viewMore() {
+        if (this.state.viewMore) {
+            this.setState({
+                viewMore: false,
+                text: "Ver más",
+            });
+        } else {
+            this.setState({
+                viewMore: true,
+                text: "Ver menos",
+            });
+        }
+    }
 
 render(){
     return(
         <section className="todo">
         <div className="container">
             
-            <div className="pre-ver-mas">
+            
                 <h3>{this.props.dataPelis.title}</h3>
                 <img src={this.state.link + this.props.dataPelis.poster_path} alt={this.props.dataPelis.title} />
                 <p>Descripcion: {this.props.dataPelis.overview}</p>
-            </div>
-            <div className="post-ver-mas">
-                <p onClick={ ()=> this.props.verMas(this.props.dataPelis.id)}>Ver Más</p>
+          
+               
+                <section
+                    className={`${this.state.viewMore ? "aditional-info-show" : "aditional-info"
+                        }`}
+                >
+                    
                     <p>Rating: {this.props.dataPelis.vote_average}</p>
                     <p>Estreno: {this.props.dataPelis.release_date}</p>
                     <p>Popularidad: {this.props.dataPelis.popularity}</p>
-                <button onClick={ ()=> this.props.borrarPeliculas(this.props.dataPelis.id)}>Borrar</button>
-           </div>
+                
+                </section>
+                <button className="more" onClick={() => this.viewMore()}>
+                    {this.state.text}
+                </button>
+                <button onClick={() => this.props.borrarPeliculas(this.props.dataPelis.id)}>Borrar</button>
+               
+                
         </div>
         </section>
     )
